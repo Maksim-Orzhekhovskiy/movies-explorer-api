43c1ac72cebbe675c01b318fe823bf02d33379d0
Maksim Orzhekhovskiy <o.maxim@magicslide.kz>
2023-06-26 10:07:36 +0600

    chore: jst options

--- controllers/users.js
+++ controllers/users.js
@@ -85,7 +85,8 @@ const login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
-        sameSite: true,
+        sameSite: 'none',
+        secure: true,
       })
         .send({ message: SIGNIN_MSG });
     })
@@ -94,7 +95,8 @@ const login = (req, res, next) => {
 
 const logout = (req, res) => {
   res.clearCookie('jwt', {
-    sameSite: true,
+    sameSite: 'none',
+    secure: true,
   });
   res.send({ message: SIGNOUT_MSG });
 };
